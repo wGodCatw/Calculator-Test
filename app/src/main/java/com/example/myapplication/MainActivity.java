69f6561b13d619ee74c8a90f886e3a2b83bd20c1
Ruslan <ruslan.yakubov05@gmail.com>
2023-09-19 21:40:42 +0300

    Calculator Apple 4.0

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         ConstraintLayout background = findViewById(R.id.background);
 
         ImageButton oneButton = findViewById(R.id.oneButton);
